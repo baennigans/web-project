d93f9856f0eeeff00431b303dcc3dd16e37d4ca8
Bae GunHee <bgh2584@gmail.com>
2023-05-09 14:09:28 +0900

    javascript 실습

---
+++ js/exam02.js
@@ -0,0 +1,17 @@
+function start() {
+    let num = prompt('숫자를 입력하세요.');
+    checkNum(num);
+}
+
+function checkNum(num) {
+    if(isNaN(num) || num===''){
+        alert('숫자를 입력해주세요!!')
+    }else{
+            if(num>=1 && num<=100){
+            for(let i=1; i<=100; i++){
+                console.log(i);
+            }
+            alert('완료되었습니다.')
+        }
+    }
+}
